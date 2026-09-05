e28c6110ea006577bfcabb236d86e6a3bdc0ccb2
Asim Mitra <52779325+AsimMitra@users.noreply.github.com>
2026-09-05 13:41:03 +0000

    initial introduction using List and print statement.

---
+++ my_practice.py
@@ -0,0 +1,3 @@
+print("Hello Folks there in codespaces, I am {}, first time here".format("Asim Mitra"))
+my_List=["Asim Mitra",20,"from CGEC"]
+print(f"I am {my_List[0]}, {my_List[1]}, I am {my_List[2]}")
