675b08d202c62782edc8cf4b94cf475b689ceb1c
Asim Mitra <52779325+AsimMitra@users.noreply.github.com>
2026-09-05 14:04:36 +0000

    practice on Dictionary and traversing keys and values with loops and finding parameter with conditional statement.

--- my_practice.py
+++ my_practice.py
@@ -1,3 +1,21 @@
 print("Hello Folks there in codespaces, I am {}, first time here".format("Asim Mitra"))
 my_List=["Asim Mitra",20,"from CGEC"]
-print(f"I am {my_List[0]}, {my_List[1]}, I am {my_List[2]}")
+print(f"I am {my_List[0]}, {my_List[1]}, I am {my_List[2]}") #formatted string with print statement
+#After first commit
+#Dictionary practice
+my_dict={"a":1,'b':2,"c":3,'d':4}   #initialization of dictionary
+# Check if a key exists
+if 'a' in my_dict:
+    print("Found a")
+
+# Loop through keys (default behavior)
+for key in my_dict.keys():  # can be used as "for key in my_dict: " without keys()
+    print(key)
+
+# Loop through values
+for value in my_dict.values():  #uses of my_dict.values()
+    print(value)
+
+# Loop through both keys and values simultaneously
+for key, value in my_dict.items():      #uses of my_dict.items()
+    print(f"key : {key}, value : {value}")
